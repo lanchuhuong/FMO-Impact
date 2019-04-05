73bb90c4f510f0155a1b815378e49ca86691f0f5
Gerrit Versteeg <gerrit@Gerrits-MacBook-Pro.local>
2019-04-05 17:19:17 +0200

    Adapted Inv% per Country to Inv$ per GHG-country

--- microservices/uS001_PE_InvPct.R
+++ microservices/uS001_PE_InvPct.R
@@ -23,35 +23,46 @@
 ## check for rows in PEFRegPct that are also in InvRegPct
 ## throw these rows out of PEFRegPct because we have better information
 ## then combine both sets and order them on Cust_id and country
-uS001_PE_InvPct <- function(investments, contracted) {
+uS001_PE_InvPct <- function(investments, contracted, countrymap) {
   require(tidyverse)
   ## 1 - Read in parameters: PEFInv, PEFREg -----------------------------------
   test <- FALSE
   if(!test) {
     PEFInv <- investments
     PEFReg <- contracted
+    lutC <- countrymap
   } 
   if(test) {
-    PEFInv <- read.csv2(fname_in_PEFInv,
-                        stringsAsFactors = FALSE, fileEncoding = "UTF-8")
-    PEFReg <- read.csv2(fname_in_PEFReg,
-                      stringsAsFactors = FALSE, fileEncoding = "UTF-8")
-  }
+    PEFInv <- read.csv2(fname_in_PEFInv, stringsAsFactors = FALSE,
+                         fileEncoding = "UTF-8")
+    PEFReg <- read.csv2(fname_in_PEFReg, stringsAsFactors = FALSE,
+                         fileEncoding = "UTF-8")
+    lutC <- read.csv2(fname_in_lutC, stringsAsFactors = FALSE,
+                       fileEncoding = "UTF-8")  }
 
-  ## 2 - calculate customer totals of investments -----------------------------
+  ## 1 - calculate customer totals of investments -----------------------------
   PEFInvCust <- PEFInv %>%
     group_by(Customer_ID) %>%
     summarise(cust_total = sum(amount)) %>%
     ungroup
 
-  ## 3 - calculate customer/country totals of investments ---------------------
+  ## 2 - In PEFInv: convert customer-countries into GHG-countries -------------
+  PEFInv <- mutate(PEFInv, GHG_country = NA)
+  for (i in seq_along(PEFInv$Customer_ID)) {
+    country <- as.character(PEFInv$country[i])
+    PEFInv$GHG_country[i] <- lutC[lutC$Country == country, 2]
+  }
+  PEFInv <- select(PEFInv, -country)
+  PEFInv <- select(PEFInv, c(1, 5, 2:4))
+  
+  ## 3 - calculate customer/GHG-country totals of investments -----------------
   PEFInvCtr <- PEFInv %>%
-    group_by(Customer_ID, country) %>%
-    summarise(ctr_total = sum(amount)) %>%
-    mutate(cust_total = 0) %>%
-    ungroup
+  group_by(Customer_ID, GHG_country) %>%
+  summarise(ctr_total = sum(amount)) %>%
+  ungroup
 
   ## 4 - Add column cust totals to investments per customer/country -----------
+  PEFInvCtr <- mutate(PEFInvCtr, cust_total = 0)
   for (i in c(1:nrow(PEFInvCtr))) {
     curcust <- as.character(PEFInvCtr[i,1])
     PEFInvCtr[i,4] <- PEFInvCust[PEFInvCust$Customer_ID == curcust,2]
@@ -63,15 +74,12 @@ uS001_PE_InvPct <- function(investments, contracted) {
   PEFInvPct <- select(PEFInvCtr, -ctr_total, -cust_total)
   rm(PEFInvCtr, PEFInvCust)
 
-  ## over to contracted regional percentages where no investments exist yet
-  ## note: countries in this part differ from countries in investments-part
-  ## PEFInv, PEFInvPct: use src_countries
-  ## PEFReg, PEFRegPct: use GHG_countries
-  
+  ## Over to contracted regional percentages where no investments exist yet
+
   ## 6 - Add status to contracted regional percentages ------------------------
   PEFRegPct <- PEFReg %>%
     add_column(status = "at_contract")
-  colnames(PEFRegPct)[2] <- "country"
+  colnames(PEFRegPct)[2] <- "GHG_country"
   colnames(PEFRegPct)[3] <- "perc"
 
   ## 7 - Remove contracted percentages if actual investments are present ------
@@ -80,24 +88,8 @@ uS001_PE_InvPct <- function(investments, contracted) {
 
   ## 8 - combine actual and contracted investement percentages ----------------
   DF_Out <- union(PEFInvPct, PEFRegPct)
-  
-  ## 9 - map percentages from country to GHG-country --------------------------
-  ## Note that lutC maps from src_country to GHG_country, but also supports
-  ## GHG_country to GHG_country, that's why we could combine Inv & Reg.
-  lutC <- read.csv2("datalake/luts/Map_CntrReg_2019-03-31.csv",
-                   stringsAsFactors = FALSE,
-                   fileEncoding = "UTF-8")
-  DF_Out <- mutate(DF_Out, GHG_country = NA)
-  for (i in c(1:nrow(DF_Out))) {
-    country <- as.character(DF_Out[i,2])
-    DF_Out[i,5] <- lutC[lutC$Country == country, 2]
-  }   
-  DF_Out <- DF_Out %>%
-    group_by(Customer_ID, GHG_country, status) %>%
-    summarise(inv_percentage = sum(perc)) %>%
-    ungroup
 
-  ## 10 - Generate output (investment-percentages per customer/country --------
+  ## 9 - Generate output (investment-percentages per customer/country --------
   Out <- arrange(DF_Out, Customer_ID, GHG_country)
   PEFInvPct <- Out
   return(Out)
