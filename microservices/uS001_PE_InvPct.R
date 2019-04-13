c0b10382458945cb609434563ed0d9d9b28d36ff
Gerrit Versteeg <gerrit@Gerrits-MacBook-Pro.local>
2019-04-13 17:54:19 +0200

    Adapted to new XLS

--- microservices/uS001_PE_InvPct.R
+++ microservices/uS001_PE_InvPct.R
@@ -21,36 +21,26 @@
 ##
 ## LOGIC
 ## check for rows in PEFRegPct that are also in InvRegPct
-## throw these rows out of PEFRegPct because we have better information
-## then combine both sets and order them on Cust_id and country
+## throw these rows out of PEFRegPct because we have actual investments
+## in InvRegPct, then combine both sets and order them on Cust_id and country
 uS001_PE_InvPct <- function(investments, contracted, countrymap) {
   require(tidyverse)
   ## 1 - Read in parameters: PEFInv, PEFREg -----------------------------------
-  test <- FALSE
-  if(!test) {
-    PEFInv <- investments
-    PEFReg <- contracted
-    lutC <- countrymap
-  } 
-  if(test) {
-    PEFInv <- read.csv2(fname_in_PEFInv, stringsAsFactors = FALSE,
-                         fileEncoding = "UTF-8")
-    PEFReg <- read.csv2(fname_in_PEFReg, stringsAsFactors = FALSE,
-                         fileEncoding = "UTF-8")
-    lutC <- read.csv2(fname_in_lutC, stringsAsFactors = FALSE,
-                       fileEncoding = "UTF-8")  }
+  PEFInv <- investments
+  PEFReg <- contracted
+  lutC <- countrymap
 
   ## 1 - calculate customer totals of investments -----------------------------
   PEFInvCust <- PEFInv %>%
     group_by(Customer_ID) %>%
-    summarise(cust_total = sum(amount)) %>%
+    summarise(cust_total = sum(outstanding)) %>%
     ungroup
 
   ## 2 - In PEFInv: convert customer-countries into GHG-countries -------------
   PEFInv <- mutate(PEFInv, GHG_country = NA)
   for (i in seq_along(PEFInv$Customer_ID)) {
-    country <- as.character(PEFInv$country[i])
-    PEFInv$GHG_country[i] <- lutC[lutC$Country == country, 2]
+    PEFInv$GHG_country[i] <- lutC$Model_Region[lutC$Country == 
+                                                 PEFInv$country[i]]
   }
   PEFInv <- select(PEFInv, -country)
   PEFInv <- select(PEFInv, c(1, 5, 2:4))
@@ -58,14 +48,14 @@ uS001_PE_InvPct <- function(investments, contracted, countrymap) {
   ## 3 - calculate customer/GHG-country totals of investments -----------------
   PEFInvCtr <- PEFInv %>%
   group_by(Customer_ID, GHG_country) %>%
-  summarise(ctr_total = sum(amount)) %>%
+  summarise(ctr_total = sum(outstanding)) %>%
   ungroup
 
   ## 4 - Add column cust totals to investments per customer/country -----------
   PEFInvCtr <- mutate(PEFInvCtr, cust_total = 0)
   for (i in c(1:nrow(PEFInvCtr))) {
-    curcust <- as.character(PEFInvCtr[i,1])
-    PEFInvCtr[i,4] <- PEFInvCust[PEFInvCust$Customer_ID == curcust,2]
+    curcust <- PEFInvCtr$Customer_ID[i]
+    PEFInvCtr[i,4] <- PEFInvCust$cust_total[PEFInvCust$Customer_ID == curcust]
   }   
 
   ## 5 - Calculate inv. percentage per cust/country and add status ------------
@@ -74,9 +64,10 @@ uS001_PE_InvPct <- function(investments, contracted, countrymap) {
   PEFInvPct <- select(PEFInvCtr, -ctr_total, -cust_total)
   rm(PEFInvCtr, PEFInvCust)
 
-  ## Over to contracted regional percentages where no investments exist yet
-
-  ## 6 - Add status to contracted regional percentages ------------------------
+  ## Over to regional asset percentages at time of contract
+  ## 6 - Remove empty asset-percentages for Cust/Region-combi's and add status
+  PEFReg <- PEFReg[!is.na(PEFReg$AssetPerc),]
+  PEFReg$AssetPerc <- as.numeric(PEFReg$AssetPerc)
   PEFRegPct <- PEFReg %>%
     add_column(status = "at_contract")
   colnames(PEFRegPct)[2] <- "GHG_country"
