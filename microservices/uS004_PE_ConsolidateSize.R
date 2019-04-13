ae75d7b2477f692847d116d7bbc4fbb3ccbf0652
Gerrit Versteeg <gerrit@Gerrits-MacBook-Pro.local>
2019-04-13 20:36:48 +0200

    Adapted to new excel (Abs GHG Acc) 2019-04-11

--- microservices/uS004_PE_ConsolidateSize.R
+++ microservices/uS004_PE_ConsolidateSize.R
@@ -28,27 +28,28 @@ uS004_PE_ConsolidateSize <- function(investments, contracted) {
   ## 2 - calculate customer totals --------------------------------------------
   PEFInvCust <- PEFInv %>%
     group_by(Customer_ID) %>%
-    summarise(cust_total = sum(amount)) %>%
+    summarise(cust_total = sum(outstanding)) %>%
     ungroup
 
   ## 3 - calculate customer/sme totals ----------------------------------------
   PEFInvSme <- PEFInv %>%
     group_by(Customer_ID, sme) %>%
     filter(sme == "Yes") %>%
-    summarise(sme_total = sum(amount)) %>%
+    summarise(sme_total = sum(outstanding)) %>%
     mutate(cust_total = 0) %>%
     ungroup
 
   ## 4 - Add column cust totals -----------------------------------------------
   for (i in c(1:nrow(PEFInvSme))) {
     curcust <- as.character(PEFInvSme[i,1])
-    PEFInvSme[i,4] <- PEFInvCust[PEFInvCust$Customer_ID == curcust, 2]
+    PEFInvSme[i,4] <- PEFInvCust$cust_total[PEFInvCust$Customer_ID == curcust]
   }   
 
   ## 5 - Calculate percentage and add status ----------------------------------
   PEFInvSme <- mutate(PEFInvSme, msme = sme_total / cust_total)
   PEFInvSme <- mutate(PEFInvSme, status = "at_review")
   PEFInvPct <- select(PEFInvSme, -sme_total, -cust_total, -sme)
+  rm(PEFInvSme)
 
   ## 6 - Transform contracted and add status ----------------------------------
   ## please note the 'strange' test on Agriculture (ref. excel-tab IDFP - E40)
@@ -57,8 +58,9 @@ uS004_PE_ConsolidateSize <- function(investments, contracted) {
   PEFCtrPct$MFI[which(is.na(PEFCtrPct$MFI))] <- 0
   PEFCtrPct$SME[which(is.na(PEFCtrPct$SME))] <- 0
   PEFCtrPct <- PEFCtrPct %>%
+    mutate(MFI = as.numeric(MFI)) %>%
     mutate(msme = (ifelse(is.na(Agriculture),NA,(SME + MFI)))) %>%
-    select(1,24) %>%
+    select(1,23) %>%
     add_column(status = "at_contract")
 
   ## 7 - Remove contracted percentages if actual investments are present ------
@@ -70,7 +72,7 @@ uS004_PE_ConsolidateSize <- function(investments, contracted) {
   DF_Out <- mutate(DF_Out, corporate = (1 - msme))
   DF_Out <- DF_Out[,c(1,2,4,3)]
 
-  ## 9 - Generate output (investment-percentages per customer/country --------
+  ## 9 - Generate output (investment-percentages per size ---------------------
   Out <- arrange(DF_Out, Customer_ID)
   ## Out <- DF_Out
   return(Out)
