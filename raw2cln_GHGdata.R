7f11120b72dbb855d62047438a316f4a38f6cd02
Gerrit Versteeg <gerrit@Gerrits-MacBook-Pro.local>
2019-04-16 15:01:01 +0200

    Corrected: Services into services

--- raw2cln_GHGdata.R
+++ raw2cln_GHGdata.R
@@ -445,6 +445,8 @@ raw$Sector_modeled[raw$Sector_modeled == "Mining & Quarrying"] <-
 raw$Sector_modeled[raw$Sector_modeled ==
                   "1/3 business services – 1/3 trade – 1/3 construction."] <-
                   "1/3 business services – 1/3 trade – 1/3 construction"  
+raw$Sector_modeled[raw$Sector_modeled == "Business Services"] <-
+  "Business services"  
 Map_SctrMdl <- raw
 write.csv2(Map_SctrMdl, fname_out_Map_SctrMdl, row.names = FALSE,
            fileEncoding = "UTF-8")
